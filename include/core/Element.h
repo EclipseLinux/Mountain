ac38f057f589ced5bf2d753846f294b76b53d557
sheerapi <crystilagoslagos945@gmail.com>
2024-02-27 20:20:13 -0300

    Added function to add children

--- include/core/Element.h
+++ include/core/Element.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "core/SignalEmitter.h"
 #include "yoga/YGConfig.h"
+#include "yoga/YGNode.h"
+#include <type_traits>
 #include <vector>
 
 namespace Mountain
@@ -9,15 +11,37 @@ namespace Mountain
 	 * @brief Base class for the whole Mountain project, allowing you to create UI objects
 	 * Also emits singlas
 	 */
-	class Element : SignalEmitter<Element>
+	class Element : public SignalEmitter<Element>
 	{
 	public:
-	protected:
-		YGNodeRef Node;
+		template <typename T, typename... Args> auto AddChild(Args&&... args) -> Element*
+		{
+			static_assert(std::is_base_of<Element, T>::value,
+						  "Expected T to be of type, or derived from Element");
 
-		/**
-		 * @brief Children of the Element, to make a tree
-		 */
-		std::vector<Element*> Children;
+			auto child = new T(args...);
+			child->_parent = this;
+			child->_init();
+			_children.push_back(child);
+
+			YGNodeInsertChild(_node, child->_node, YGNodeGetChildCount(_node));
+			EmitSignal("childNew", child);
+
+			return this;
+		}
+
+	protected:
+		virtual void Init(){};
+
+	private:
+		Element* _parent;
+		std::vector<Element*> _children;
+		YGNodeRef _node;
+
+		void _init()
+		{
+			_node = YGNodeNew();
+			Init();
+		}
 	};
 }
